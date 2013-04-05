34f791cd84af6a921307856571e85b7622fd2402
郑硕 <zhengshuo@zhengshuotekiMacBook-Air.local>
2013-04-05 22:35:19 +0800

    改版了
    改版了

--- Inc/GG_336.js
+++
@@ -1,8 +0,0 @@
-google_ad_client = "ca-pub-4026748263627641";
-/* rd336 */
-google_ad_slot = "6544890296";
-google_ad_width = 336;
-google_ad_height = 280;
-
-document.write('<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>');
-
