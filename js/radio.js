eb78d6a96c53a0bd637530e1e6c0c4eeeb77ba9c
amswf <zhengshuo@outlook.com>
2013-05-13 16:33:31 +0800

    修复bug

--- js/radio.js
+++ js/radio.js
@@ -2,69 +2,53 @@
  * ...
  * @author zhengshuo
  */
-
 (function() {
 	var data = [{id:"n1",text:"热门电台",expanded:true},
-                     {id:"n2",text:"按地区"},
-                     {id:"n11",pid:"n1",text:"CRI怀旧金曲",name:'dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1043389200%26start%3D1367510401%26end%3D1372662685&amp;volumn=0.5&amp;uid=1052710881&amp;eid=1043389200&amp;pageURL=http://radio.weibo.com/online/crionline2012?source=radioarea_lastlisten'},
-			         {id:"n12",pid:"n1",text:"中央人民广播电台中国之声-FM106.1",name:'dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1043387760%26start%3D1367510401%26end%3D1370334002&volumn=0.5&uid=1052710881&eid=1043387760&pageURL=http://radio.weibo.com/china/fm1061?source=radio_change'},
-			         {id:"n13",pid:"n1",text:"中央人民广播电台音乐之声-FM90.0",name:'dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1043390840%26start%3D1367510401%26end%3D1373874601&volumn=0.5&uid=1052710881&eid=1043390840&pageURL=http://radio.weibo.com/china/fm900?source=radio_change'},
-			         {id:"n14",pid:"n1",text:"中央人民广播电台经济之声-FM96.6",name:'dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1042929490%26start%3D1367510401%26end%3D1371297268&volumn=0.5&uid=1052710881&eid=1042929490&pageURL=http://radio.weibo.com/china/fm966?source=radio_change'},
-			         {id:"n21",pid:"n2",text:"移动"},
-			         {id:"n22",pid:"n2",text:"联通"},
-			         {id:"n23",pid:"n2",text:"电信"}];
-	window.get = function(id){
-		return window.document.getElementById(id);
+			 {id:"n2",text:"按地区"},
+			 {id:"n11",pid:"n1",text:"CRI怀旧金曲",name:'dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1043389200%26start%3D1367510401%26end%3D1372662685&amp;volumn=0.5&amp;uid=1052710881&amp;eid=1043389200&amp;pageURL=http://radio.weibo.com/online/crionline2012?source=radioarea_lastlisten'},
+			 {id:"n12",pid:"n1",text:"中央人民广播电台中国之声-FM106.1",name:'dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1043387760%26start%3D1367510401%26end%3D1370334002&volumn=0.5&uid=1052710881&eid=1043387760&pageURL=http://radio.weibo.com/china/fm1061?source=radio_change'},
+			 {id:"n13",pid:"n1",text:"中央人民广播电台音乐之声-FM90.0",name:'dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1043390840%26start%3D1367510401%26end%3D1373874601&volumn=0.5&uid=1052710881&eid=1043390840&pageURL=http://radio.weibo.com/china/fm900?source=radio_change'},
+			 {id:"n14",pid:"n1",text:"中央人民广播电台经济之声-FM96.6",name:'dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1042929490%26start%3D1367510401%26end%3D1371297268&volumn=0.5&uid=1052710881&eid=1042929490&pageURL=http://radio.weibo.com/china/fm966?source=radio_change'},
+			 {id:"n21",pid:"n2",text:"移动"},
+			 {id:"n22",pid:"n2",text:"联通"},
+			 {id:"n23",pid:"n2",text:"电信"}];
+			 
+	function get(id){
+		return document.getElementById(id);
 	}
 	
-	function getIEVersion(){
-		var v;
-		 if (document.documentMode == 10)
-			v = 10;
-		 else if (document.documentMode == 9)
-			v = 9;
-		 else if (window.postMessage)
-			v = 8;
-		 else if (window.XMLHttpRequest)
-			v = 7;
-		 else if (document.compatMode)
-			v = 6;
-		 else if (window.createPopup)
-			v = 5.5;
-		 else if (window.attachEvent)
-			v = 5;
-		 else if (document.all){
-			v = 4;
-		 }
-		return v;
-	}
-	
-	window.getFlash = function(id){
-		if (window.navigator.appName.indexOf("Microsoft") != -1) {
-			if(document.documentMode == 10){
-				return window.document.getElementById(id);
+	function getFlash(id){
+		if (navigator.appName.indexOf("Microsoft") != -1) {
+			if(document.documentMode == 10 || document.documentMode == 9){
+				return document.getElementById(id);
 			}else{
-				window[id];
+				return window[id];
 			}
-        } else {
-            return document[id];
-        }
+		} else {
+			return document[id];
+		}
 	}
 	
 	window.radioReady = function(){
-	    //alert(getFlash('radioAudio'));
-		//alert(getFlash('radioAudio').play);
-		getFlash('radioAudio').play('dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1043389200%26start%3D1367510401%26end%3D1372662685&amp;volumn=0.5&amp;uid=1052710881&amp;eid=1043389200&amp;pageURL=http://radio.weibo.com/online/crionline2012?source=radioarea_lastlisten');
+		setTimeout(create,100);
+	}
+	
+	function create(){
+		getFlash('radioAudio').goplay('dataURL=http%3A%2F%2Fcdn.kandian.com%2Fmovies%3Fcmd%3Dplay%26id%3D1043389200%26start%3D1367510401%26end%3D1372662685&amp;volumn=0.5&amp;uid=1052710881&amp;eid=1043389200&amp;pageURL=http://radio.weibo.com/online/crionline2012?source=radioarea_lastlisten');
 		var playBtn = get('playBtn');
 		var volume = 0.5;//音量
-		var isPlay;//是否在播放
+		var isPlay = true;//是否在播放
+		var isMute = false;
 		playBtn.onclick = function(){
 			if(playBtn.className == 'ico_rdoPlay ico_stop_gray'){
 				playBtn.className = 'ico_rdoPlay ico_play_gray';
 				getFlash('radioAudio').setVolume(0);
+				isPlay = false;
 			}else{
 				playBtn.className = 'ico_rdoPlay ico_stop_gray';
-				getFlash('radioAudio').setVolume(volume);
+				if(!isMute)
+					getFlash('radioAudio').setVolume(volume);
+				isPlay = true;
 			}
 		}
 		var muteBtn = get('muteBtn');
@@ -74,11 +58,14 @@
 				getFlash('radioAudio').setVolume(0);
 				get('volume').style.width = '0px';
 				volumeBtn.style.left = "0px";
+				isMute = true;
 			}else{
 				muteBtn.className = 'ico_rdoPlay ico_sound_on';
-				getFlash('radioAudio').setVolume(volume);
+				if(isPlay)
+					getFlash('radioAudio').setVolume(volume);
 				volumeBtn.style.left = volume*100 + "px";
 				get('volume').style.width = volume*100 + 'px';
+				isMute = false;
 			}
 		}
 		var volumeBtn = get('radio_vol');
@@ -118,7 +105,7 @@
 			simpleDataModel: true,
 			onSelect: function(node,event) {
 				playBtn.className = 'ico_rdoPlay ico_stop_gray';
-				getFlash('radioAudio').play(node.name);
+				getFlash('radioAudio').goplay(node.name);
 			}
 		});
 	}
